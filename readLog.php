db4ab13ccd98a7ce945a67cdc02826ba8c24924e
baxtex stat <baxtexx@gmail.com>
2016-10-22 09:37:21 +0200

    Added local log file

--- readLog.php
+++ readLog.php
@@ -2,7 +2,8 @@
 class readLog{
 	function getLog(){
 		header('Content-Type: application/json');
-		$file = file_get_contents('../../../home/shares/flower_hum/humid.log');
+		//$file = file_get_contents('../../../home/shares/flower_hum/humid.log');
+		$file = file_get_contents('humid.log');
 		$lengthOfFile = strlen($file)-2;
 		$subFile = substr($file, 0, $lengthOfFile);
 		$res ="[";
