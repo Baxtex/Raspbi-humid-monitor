1627545d1973c33b1197ccbe2a814330397acc14
baxtex stat <baxtexx@gmail.com>
2016-10-22 09:47:05 +0200

    Added smaller update

--- readLog.php
+++ readLog.php
@@ -2,8 +2,8 @@
 class readLog{
 	function getLog(){
 		header('Content-Type: application/json');
-		//$file = file_get_contents('../../../home/shares/flower_hum/humid.log');
-		$file = file_get_contents('humid.log');
+	
+		$file = file_get_contents('humid.txt');
 		$lengthOfFile = strlen($file)-2;
 		$subFile = substr($file, 0, $lengthOfFile);
 		$res ="[";
